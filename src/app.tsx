403c1173a899eb2dbe3f743e593391f43583f307
monodyle <monodylh@gmail.com>
2025-03-11 21:25:36 +0700

    screen

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,24 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>
+  return (
+    <main className="flex flex-col min-h-screen bg-base">
+      <div className="flex flex-col flex-1 w-full max-w-2xl px-5 py-4 mx-auto">
+        <div className="flex flex-col items-center justify-center flex-1 w-full mx-auto space-y-4 border border-border rounded-xl">
+          hello!
+        </div>
+      </div>
+      <footer className="px-5 py-4">
+        <p className="text-sm text-center text-primary/60">
+          made with ❤️ by{' '}
+          <a
+            href="https://github.com/monodyle"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            monodyle
+          </a>
+        </p>
+      </footer>
+    </main>
+  )
 }
