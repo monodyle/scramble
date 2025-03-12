2e550ba7dd81ae1ac14cb6b02f8c5c31b3eb89a6
monodyle <monodylh@gmail.com>
2025-03-12 22:40:47 +0700

    add open graph image

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -9,6 +9,29 @@ export default defineConfig({
       {
         tag: 'link',
         attrs: {
+          rel: 'icon',
+          href: '/favicon.ico',
+        },
+      },
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'icon',
+          href: '/favicon.png',
+          type: 'image/png',
+          sizes: '32x32',
+        },
+      },
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'apple-touch-icon',
+          href: '/apple-touch-icon.png',
+        },
+      },
+      {
+        tag: 'link',
+        attrs: {
           rel: 'preconnect',
           href: 'https://fonts.googleapis.com',
         },
@@ -33,6 +56,76 @@ export default defineConfig({
         attrs: {
           href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..900;1,100..900&display=swap',
           rel: 'stylesheet',
+        },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'og:type',
+          content: 'website',
+        },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'og:title',
+          content: 'Scramble Word!',
+        },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'og:description',
+          content: 'Guess the scrambled word!',
+        },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'og:url',
+          content: 'https://scramble.takea.cafe',
+        },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'og:image',
+          content: 'https://scramble.takea.cafe/preview.jpg',
+        },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'twitter:card',
+          content: 'summary_large_image',
+        },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'twitter:url',
+          content: 'https://scramble.takea.cafe',
+        },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'twitter:title',
+          content: 'Scramble Word!',
+        },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'twitter:description',
+          content: 'Guess the Chaos!',
+        },
+      },
+      {
+        tag: 'meta',
+        attrs: {
+          property: 'twitter:image',
+          content: 'https://scramble.takea.cafe/preview.jpg',
         },
       },
     ],
